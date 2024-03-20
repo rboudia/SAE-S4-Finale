a33c9b97c8a7e540324b21a90c8da30a6aeae038
rboudia <rboudia@iut.univ-paris8.fr>
2024-03-20 17:57:58 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {TournoiComponent} from "./tournoi/tournoi.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'tournois', component: TournoiComponent},
+];
