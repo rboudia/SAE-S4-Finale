8238a0a6cefbfd676180a3b6f82fc05df4939a7f
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2024-03-25 14:15:07 +0100

    .

--- src/app/services/joueur.service.ts
+++ src/app/services/joueur.service.ts
@@ -12,7 +12,10 @@ export class JoueurService {
   ajouterJoueur(joueurData: any): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://127.0.0.1:5000',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
       })
     };
 
