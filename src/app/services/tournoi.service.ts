ef32dcab1972469b7a089b1677cf4437d3a91c11
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2024-03-27 11:00:29 +0100

    modifs services tournois pour aff joueurs

--- src/app/services/tournoi.service.ts
+++ src/app/services/tournoi.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,9 @@ export class TournoiService {
 
   getTournois() {
     return this.http.get("/api/tournois")
+  }
+
+  getJoueurs(): Observable<any[]> {
+    return this.http.get<any[]>("/api/joueurs");
   }
 }
