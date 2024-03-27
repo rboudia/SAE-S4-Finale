83c598f2b91d900c65417a36489badaae84f6e0f
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2024-03-27 11:14:58 +0100

    ajouts ids joueurs ds component tournois

--- src/app/tournoi/tournoi.component.ts
+++ src/app/tournoi/tournoi.component.ts
@@ -15,30 +15,10 @@ export class TournoiComponent {
 
   items: any;
   showItems = false;
-  tournois: any[] | undefined;
-  joueurs: any[] | undefined;
 
 
   constructor(private http: HttpClient, private serviceTournoi: TournoiService) {
     this.getItems();
-    this.getJoueurs();
-  }
-
-  getJoueurs() {
-    this.serviceTournoi.getJoueurs().subscribe(
-      data => {
-        this.joueurs = data;
-      },
-      erreur => {
-        console.error('Erreur lors de la récupération des joueurs!', erreur);
-      }
-    );
-  }
-
-  getNomJoueur(idJoueur: string): string {
-    // @ts-ignore
-    const joueur = this.joueurs.find(joueur => joueur._id === idJoueur);
-    return joueur ? joueur.nom + ' ' + joueur.prenom : 'Joueur inconnu';
   }
 
   getItems() {
