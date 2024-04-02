1a1aa64096d06f7c6bf1178cc34cb764340cb8ac
aboudjemai <aboudjemai@iut.univ-paris8.fr>
2024-04-02 16:49:36 +0200

    component joueur et tournoi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import {RouterLink, RouterOutlet} from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'SAR_S4';
